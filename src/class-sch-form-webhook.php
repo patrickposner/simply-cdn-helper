289c5063039e9dcccd560bfc7cea7a1f94d648ba
Patrick Posner <hello@patrickposner.dev>
2022-05-20 14:34:32 +0200

    disable ajax in CF7 and GF

--- src/class-sch-form-webhook.php
+++ src/class-sch-form-webhook.php
@@ -14,34 +14,34 @@ class Form_Webhook {
 	private static $instance = null;
 
 	/**
-	 * Returns instance of CDN.
-	 *
-	 * @return object|null
-	 */
-	public static function get_instance() {
-
-		if ( null === self::$instance ) {
-			self::$instance = new self();
-		}
-
-		return self::$instance;
-	}
-
-	/**
-	 * Constructor for CDN.
+	 * Constructor for Form_Webhook.
 	 *
 	 * @return void
 	 */
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'add_webhook_scripts' ) );
+		add_filter( 'wpcf7_load_js', '__return_false' );
+		add_filter( 'gform_form_args', array( $this, 'disable_ajax' ) );
 	}
 
 	/**
-	 * Enqueue scripts for webhooks.
+	 * Enqueue scripts for webhook.
 	 *
 	 * @return void
 	 */
 	public function add_webhook_scripts() {
-		wp_enqueue_script( 'sch-form-webhook-js', SCH_URL . '/assets/sch-form-webhook.js', array(), '1.0', true );
+		wp_enqueue_script( 'sch-webhook-js', SCH_URL . '/assets/sch-webhook.js', array(), '1.0', true );
+	}
+
+	/**
+	 * Disable ajax in Gravity Forms.
+	 *
+	 * @param array $args given list or arguments.
+	 *
+	 * @return mixed
+	 */
+	public function disable_ajax( $args ) {
+		$args['ajax'] = false;
+		return $args;
 	}
 }
